2af63b04969509e7d43dd915637bdc2d478a3f15
Pankaj Kumar <pankaj.kumar@claysol.com>
2022-06-28 02:52:31 +0530

    Diff file is modify

--- Diff.java
+++ Diff.java
@@ -1,5 +1,8 @@
 class Diff{
     public static void main(String[] args) {
         System.out.println("Hello Hello");
+        int a =5;
+        int b =3;
+        System.out.println(a-b);
     }
 }
