0d56ccd9ceeae1e3af43a2d083ba4702e5118934
Pankaj Kumar <pankaj.kumar@claysol.com>
2022-06-28 03:26:41 +0530

    Try catch blockm is added

--- Sum.java
+++ Sum.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 class Java{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please Enter your First Number : ");
-        int a = sc.nextInt();
-        System.out.println("Please Enter you second Number : ");
-        int b = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Please Enter your First Number : ");
+            int a = sc.nextInt();
+            System.out.println("Please Enter you second Number : ");
+            int b = sc.nextInt();
 
-        System.out.println(a+b);
+            System.out.println(a+b);
+        }
     }
 }
