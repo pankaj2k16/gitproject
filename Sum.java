76af67961f0e539f3a03ba04e24946033c2f638b
Pankaj Kumar <pankaj.kumar@claysol.com>
2022-06-28 02:50:38 +0530

    Taking input from user

--- Sum.java
+++ Sum.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Java{
     public static void main(String args[]){
-        int a = 45;
-        int b = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter your First Number : ");
+        int a = sc.nextInt();
+        System.out.println("Please Enter you second Number : ");
+        int b = sc.nextInt();
 
         System.out.println(a+b);
     }
